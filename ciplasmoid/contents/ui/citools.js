beb1abd53e084bf0c36af6dc0b3a2488513df44d
Christian Oschwald <christian.oschwald@txtr.com>
2013-02-28 16:33:45 +0100

    building

--- ciplasmoid/contents/ui/citools.js
+++ ciplasmoid/contents/ui/citools.js
@@ -4,6 +4,7 @@ var OKCodes = ["stable", "back to normal", "?"];
 function handleItems(items) {
 	var jenkinsTitleRE = /(.*) #[0-9]+ \((.*)\)/;
 	var allOK = true;
+	var building = false;
 	root.building = false;
 	for (var i in items) {
 		var result = jenkinsTitleRE.exec(items[i].title);
@@ -12,14 +13,17 @@ function handleItems(items) {
 			if (OKCodes.indexOf(result[2]) == -1) {
 				allOK = false;
 			}
-			if ("?"==result[2]) {
-				root.building = true;
+			if ( result[2].localeCompare("?")==0 ) {
+				building = true;
+				console.debug("handleItems: building")
 			}
 		}
 	}
 	
 	root.allOK = !allOK
 	root.allOK = allOK
+	root.building = !building
+	root.building = building
 }
 
 function setIconName(source) {
